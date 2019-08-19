913691b409b73203cafb29495963587459340ecc
raejimenezca <raejimenezca@unal.edu.co>
2019-08-18 22:22:53 -0500

    Creadas las actividades para ver la lista de productos, productos agregados al carrito y configuraciones de la cuenta

---
+++ app/src/main/java/example/tugangazo/com/Interface/ItemClickListener.java
@@ -0,0 +1,8 @@
+package example.tugangazo.com.Interface;
+
+import android.view.View;
+
+public interface ItemClickListener {
+
+    void onClick (View view, int position, boolean isLongClick);
+}
--- app/src/main/java/example/tugangazo/com/LoginActivity.java
+++ app/src/main/java/example/tugangazo/com/LoginActivity.java
@@ -126,12 +126,13 @@ public class LoginActivity extends AppCompatActivity {
                                 loadingBar.dismiss();
 
                                 Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                                Prevalent.currentOnlineUser = usersData;
                                 startActivity(intent);
                             }
                         }
                         else {
                             loadingBar.dismiss();
-                            Toast.makeText(LoginActivity.this, "Contraseña valida", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(LoginActivity.this, "Contraseña no valida", Toast.LENGTH_SHORT).show();
                         }
                     }
                 }
